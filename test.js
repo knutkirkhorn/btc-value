0f4376fdc3eb743441710278e8462dd92b13be7f
Knut Kirkhorn <knutkirk@hotmail.com>
2017-12-22 11:26:59 +0100

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const btcValue = require('./');
-
-btcValue(2.2).then((value) => {
-    console.log('$' + value);
-});
-
-btcValue.getConvertedValue('NOK').then((value) => {
-    console.log('kr' + value);
-});
