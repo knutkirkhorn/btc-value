4ed9e69a46b9606a0a156198b33a38d1662cd591
Knut Kirkhorn <knutkirk@hotmail.com>
2019-04-12 16:37:31 +0200

    Add more tests with mock of server responses

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import nock from 'nock';
 import btcValue from '.';
 
 test('value return something', t => {
@@ -164,4 +165,129 @@ test('return Error when `currencyCode` is not a valid one', t => {
     }).catch(error => {
         t.deepEqual(error, expectedResult);
     });
+});
+
+test('server return invalid data response', t => {
+    const expectedResult = TypeError('Cannot read property \'price_usd\' of undefined');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(200, {
+            invalid_response: 'here'
+        });
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return non-JSON response', t => {
+    const expectedResult = Error('Failed to retrieve Bitcoin value');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(200, 'no_json here');
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return invalid HTTP status code (123)', t => {
+    const expectedResult = Error('Failed to load url: 123');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(123, {
+            invalid_response: 'here'
+        });
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return invalid HTTP status code (1337)', t => {
+    const expectedResult = Error('Failed to load url: 1337');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(1337, {
+            invalid_response: 'here'
+        });
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return error code', t => {
+    const expectedResult = {
+        errno: 'ECONNREFUSED',
+        code: 'ECONNREFUSED',
+        syscall: 'connect'
+    };
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .replyWithError({
+            errno: 'ECONNREFUSED',
+            code: 'ECONNREFUSED',
+            syscall: 'connect'
+        });
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return missing data (missing `price_usd`)', t => {
+    const expectedResult = Error('Failed to retrieve Bitcoin value');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(200, [
+            {
+                missing_data: 'here'
+            }
+        ]);
+
+    return btcValue().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test('server return missing data (missing percent change)', t => {
+    const expectedResult = Error('Failed to retrieve percentage change');
+
+    nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/bitcoin/')
+        .reply(200, [
+            {
+                missing_data: 'here'
+            }
+        ]);
+
+    return btcValue.getPercentageChangeLastWeek().then(() => {
+        t.fail();
+    }).catch(error => {
+        t.deepEqual(error, expectedResult);
+    });
+});
+
+test.after(() => {
+    // Clean all nocks
+    nock.cleanAll();
 });
